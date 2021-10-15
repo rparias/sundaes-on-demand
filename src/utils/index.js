3caf9d0cc04e84fabbc5593e04d0adeca4cb10d1
Ronald Arias <ariasron@hotmail.com>
2021-10-15 15:36:08 -0500

    Creates a function that format the currency of an amount

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 export function convertFirstLetterToUpperCase(string) {
   return string[0].toUpperCase() + string.slice(1).toLowerCase()
 }
+
+export function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(amount)
+}
